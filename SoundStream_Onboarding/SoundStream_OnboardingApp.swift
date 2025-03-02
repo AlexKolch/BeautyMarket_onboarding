7f63a87de7b4ef31515a5a91dff3cd1716b4fa5e
Alex Kolch <alexkolch.ios@gmail.com>
2025-03-02 15:07:13 +0300

    Initial Commit

---
+++ SoundStream_Onboarding/SoundStream_OnboardingApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoundStream_OnboardingApp.swift
+//  SoundStream_Onboarding
+//
+//  Created by Алексей Колыченков on 02.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct SoundStream_OnboardingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
